678b83f1c8fccc95eca9644deb69ee343359fb01
yejee1228 <yejee1228@gmail.com>
2022-08-29 16:45:26 +0900

    baseurl 수정

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clientApi = axios.create({
-  baseURL: "http://mbti-env.eba-w4bnzjmm.ap-northeast-2.elasticbeanstalk.com/",
+  baseURL: "/",
 });
 
 export default clientApi;
